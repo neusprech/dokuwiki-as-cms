353a13b6dcabef5dcaefb9ae118734de8e74d769
neusprech <benedict.stuart@googlemail.com>
2013-12-09 05:21:54 -0800

    acl+user file

---
+++ htdocs/conf/users.auth.php
@@ -0,0 +1,11 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+user:5f4dcc3b5aa765d61d8327deb882cf99:Us Er:none@local:user
